2d15a509214f7db2e3e53e2b64f5523683d9595f
Kakawm005 <kayquespimentel1@gmail.com>
2026-06-12 12:27:18 -0300

    Melhorias no código

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -6,8 +6,9 @@ app = Flask(__name__)
 def homepage():
     return render_template("homepage.html")
 
-def perfil():
-    return "perfil do usuario"
+@app.route("/profile/<usuario>")
+def profile(usuario):
+    return render_template("profile.html", usuario=usuario)
 
 if __name__ == "__main__":
     app.run(debug=True)
