211c28f32e0445858ff58a841ab11565285646cb
Kakawm005 <kayquespimentel1@gmail.com>
2026-06-13 17:30:24 -0300

    Altera arquivos para adaptar a banco de dados, e formulario de login

--- fakepinterest/forms.py
+++ fakepinterest/forms.py
@@ -1,1 +1,24 @@
 # Criar os formularios do nosso site
+from flask_wtf import flaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from fakepinterest.models import Usuario
+
+
+class FormLogin(flaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    senha = PasswordField("Senha", validators=[DataRequired()])
+    botao_confirmacao = SubmitField("Fazer Login")
+
+
+class FormCriarConta(flaskForm):
+    email = StringField("E-Mail", validators=[DataRequired(), Email()])
+    username = StringField("UserName", validators=[DataRequired()])
+    senha = PasswordField("Senha", validators=[DataRequired(), Length(6, 20)])
+    confirmacao_senha = PasswordField("Confirmação de Senha", validators=[DataRequired(), EqualTo("senha")])
+    botao_confirmacao = SubmitField("Criar Conta")
+
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if usuario:
+            return ValidationError("E-mail ja cadastrado, faça login para continuar")
--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -1,11 +1,15 @@
+# criar as rotas do nosso site (os links)
 from flask import Flask, render_template, url_for
 from fakepinterest import app
-# Criar o banco de dados
+from flask_login import login_required
+
+
 
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
 
 @app.route("/profile/<usuario>")
+@login_required
 def profile(usuario):
     return render_template("profile.html", usuario=usuario)
