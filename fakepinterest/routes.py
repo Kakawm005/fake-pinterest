a9f604f39b947f5879edd942798605bf11dbd737
Kakawm005 <kayquespimentel1@gmail.com>
2026-06-17 13:08:46 -0300

    criar rota de cadastro, criar criptografia para senha de usuario e adicionar ao banco de dados, cria pagina de cadastro

--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -1,21 +1,30 @@
 # criar as rotas do nosso site (os links)
 from flask import Flask, render_template, url_for
-from fakepinterest import app
+from fakepinterest import app, database, bcrypt
 from flask_login import login_required
 from fakepinterest.forms import FormLogin, FormCadastro
-
+from fakepinterest.models import Usuario, Foto
 
 
 @app.route("/", methods=["GET", "POST"])
 def homepage():
+    return render_template("homepage.html")
+
+
+@app.route("/login")
+def login():
     formlogin = FormLogin()
-    return render_template("homepage.html", form=formlogin)
+    return render_template("login.html", form=formlogin)
 
 
-@app.route("/Cadastro", methods=["GET", "POST"])
+@app.route("/cadastro", methods=["GET", "POST"])
 def cadastro():
     formcadastro = FormCadastro()
-    render_template("cadastro.html", form=formcadastro)
+    if formcadastro.validate_on_submit():
+        usuario = Usuario(username=formcadastro.username.data, 
+                          email=formcadastro.email.data,
+                          senha=formcadastro.senha.data)
+    return render_template("cadastro.html", form=formcadastro)
 
 
 @app.route("/profile/<usuario>")
